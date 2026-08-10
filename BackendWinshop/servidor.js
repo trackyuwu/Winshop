07bb3d1c8334247ac8c17cf522d56dbda9833942
Celeste <gmigue523@gmail.com>
2026-08-10 16:55:56 -0500

    Se agrego el frontend

--- BackendWinshop/servidor.js
+++ BackendWinshop/servidor.js
@@ -2,11 +2,13 @@ const express = require("express");
 const { MongoClient, ObjectId } = require("mongodb");
 const cors = require("cors");
 const bcrypt = require("bcrypt");
+const path = require('path');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '../frontend')));
 
 const url = process.env.MONGO_URI || "mongodb://localhost:27017";
 const client = new MongoClient(url);
